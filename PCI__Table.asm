9e383613c53844f2f81c3e4fdc57ce112d3e888a
harrimand <24970005+harrimand@users.noreply.github.com>
2024-04-01 10:08:19 -0700

    Added Comments

--- PCI__Table.asm
+++ PCI__Table.asm
@@ -1,6 +1,7 @@
 
-; Pin Change Interrupt on PORTA3..0
-; Output 7 segment to PORTB
+; Select a function and decoded seven segment output
+; using Pin Change Interrupt on PORTA3..0
+; Output 7 segment to PORTB using selected function
 
 .nolist
 .include "m1284Pdef.inc"
@@ -8,6 +9,8 @@
 
 .def 	TEMP = R16
 .def 	bitCount = R18
+.def	sevSegOut = R20
+.equ	SevSegDecode = SRAM_START + $10
 
 .ORG	$0000
 		rjmp	RESET
@@ -22,6 +25,13 @@ RESET:
 
 		eor 	TEMP, TEMP
 		out 	DDRA, TEMP  ;PortA3..0 INPUT
+		ldi 	TEMP, $0F ; Pull-Ups on Bits 3..0
+		out 	PORTA, TEMP
+
+		; To simulate Pull-Ups set PINA 3..0 High
+		; Change Pin to Low after global interrupts
+		; enabled to simulate button press
+
 		ser 	TEMP
 		out 	DDRB, TEMP  ;PortB7..0 OUTPUT
 
@@ -45,25 +55,36 @@ MAIN:
 FindButton:
 		in  	TEMP, PINA
 		andi	TEMP, $0F ;Mask out bits 7..4		
+		; Return if no button pressed
+		cpi 	TEMP, $0F
+		brne	Pressed
+		reti
+Pressed:
 		clr 	bitCount
-NextBit:
+NextBit:  ;Logical Shift Right until carry clear and count shifts
 		inc 	bitCount
 		lsr 	TEMP
 		brcs	NextBit
 		dec 	bitCount
-		ldi 	XH, high(SRAM_START + $10)
-		ldi 	XL, low(SRAM_START + $10)
+		ldi 	XH, high(SevSegDecode)
+		ldi 	XL, low(SevSegDecode)
 		add 	XL, bitCount
-		ld  	TEMP, X
-		ldi 	ZH, high(FunAddresses)
-		ldi 	ZL, low(funAddresses)
-		lsl 	bitCount
-		add 	ZL, bitCount				
+		ld  	sevSegOut, X	; Get decoded Seven Segment Data
+
+		lsl 	bitCount ; Double bitCount to index words.
+
+		ldi 	ZH, high(FunAddresses<<1) ;Load function table base address
+		ldi 	ZL, low(funAddresses<<1)
+		add 	ZL, bitCount  ; add offset to select function address
+		lpm 	XL, Z+ ; Load address into X using Z
+		lpm 	XH, Z
+		movw	ZH:ZL, XH:XL ; Copy X to Z
+		icall	; Call function at address in Z 
 		reti
 
 CopyTable:
-		ldi 	ZH, high(SevSeg<<1)
-		ldi 	ZL, low(SevSeg<<1)
+		ldi 	ZH, high(SevSegTable<<1)
+		ldi 	ZL, low(SevSegTable<<1)
 		ldi 	YH, high(SevSegEnd<<1)
 		ldi 	YL, low(SevSegEnd<<1)
 		ldi  	XH, high(SRAM_START + $10)
@@ -76,7 +97,7 @@ CopyNext:
 		brne	CopyNext
 		ret
 
-SevSeg:
+SevSegTable:
 .db		$3F, $06, $5B, $4F, $66, $6D, $7D, $07, $7F, $6F
 SevSegEnd:
 
@@ -86,21 +107,21 @@ FunEnd:
 	
 Fun0:
 	nop
-	out PORTB, TEMP
+	out PORTB, sevSegOut
 	nop
 	ret
 Fun1:
 	nop
-	out PORTB, TEMP
+	out PORTB, sevSegOut
 	nop
 	ret
 Fun2:
 	nop
-	out PORTB, TEMP
+	out PORTB, sevSegOut
 	nop
 	ret
 Fun3:
 	nop
-	out PORTB, TEMP
+	out PORTB, sevSegOut
 	nop
 	ret
